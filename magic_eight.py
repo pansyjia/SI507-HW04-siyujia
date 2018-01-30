09a3dcc6836c8d54d1f0c62f7167c9733d0865c4
pansyjia <siyujia@umich.edu>
2018-01-30 13:31:49 -0500

    ? and quit

--- magic_eight.py
+++ magic_eight.py
@@ -1,4 +1,11 @@
 def question():
-	q = input("What is your question?")
+    user_input = ""
+    while user_input != "quit":
+        user_input = input("What is your question? Or, enter 'quit' to exit.")
 
-	
+        if user_input == "quit":
+            break
+        elif user_input[-1] != "?":
+            print("I’m sorry, I can only answer questions ended with '?'.")
+
+question()
